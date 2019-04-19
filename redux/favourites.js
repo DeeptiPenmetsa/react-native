4f1607b64b4fbc980b72da8b34811fa0b797b60e
deepti.penmetsa <deepti.penmetsa@bluepal.com>
2019-04-19 16:00:19 +0530

    Redux with React adding favorites minor change

--- redux/favourites.js
+++ redux/favourites.js
@@ -2,7 +2,7 @@ import * as ActionTypes from './ActionTypes';
 
 export const favorites = (state = [], action) => {
     switch (action.type) {
-        case ActionTypes.ADD_FAVOURITE:
+        case ActionTypes.ADD_FAVORITE:
             if (state.some(el => el === action.payload))
                 return state;
             else
