24a483484d9ef3fbea264f6479fc7fbf50798b0d
deepti.penmetsa <deepti.penmetsa@bluepal.com>
2019-04-20 10:42:49 +0530

    persistStore in redux

--- App.js
+++ App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import Main from './components/MainComponent'
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/ConfigureStore';
+import { Loading } from './components/LoadingComponent';
+import { PersistGate } from 'redux-persist/es/integration/react'
 
-const store = ConfigureStore();
+const {persistor,store} = ConfigureStore();
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Main />
+        <PersistGate 
+          loading={<Loading />}
+          persistor={persistor}>
+          <Main />
+        </PersistGate>
       </Provider>
     );
   }
